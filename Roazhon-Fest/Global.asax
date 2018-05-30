e1b6e27a119834e7fa87d1014f3c60e531895f26
nsanquer2017 <nsanquer2017@22-UC27-25>
2018-05-30 11:51:16 +0200

    first commit

---
+++ Roazhon-Fest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Roazhon_Fest.MvcApplication" Language="C#" %>
---
+++ Roazhon-Fest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Roazhon_Fest.Startup))]
+namespace Roazhon_Fest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
