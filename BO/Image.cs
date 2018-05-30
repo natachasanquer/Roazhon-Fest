2ae719ec7da6629f76b076e0d36adb3b77abfb62
pcormier2017 <pcormier2017@22-UC25-16>
2018-05-30 15:50:47 +0200

    Création couche BO #3 (pierre)

---
+++ BO/Image.cs
@@ -0,0 +1,21 @@
+﻿using BO;
+using System;
+
+namespace BO
+{
+    public class Image : IEntityIdentifiable
+    {
+        public Image()
+        {
+        }
+
+        public Image(Guid id, string url) : this()
+        {
+            this.ID = id;
+            this.Url = url;
+        }
+
+        public Guid ID { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ BO/Theme.cs
@@ -0,0 +1,22 @@
+﻿using BO;
+using System;
+
+namespace BO
+{
+    public class Theme : IEntityIdentifiable
+    {
+        public Theme()
+        {
+        }
+
+        public Theme(Guid id, string libelle) : this()
+        {
+            this.ID = id;
+            this.Libelle = libelle;
+        }
+
+        public Guid ID { get; set; }
+        public string Libelle { get; set; }
+    }
+
+}
