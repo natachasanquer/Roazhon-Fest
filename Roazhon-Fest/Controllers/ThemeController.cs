2dad04ded7279c1b6e6157111d4041e4de675504
nsanquer2017 <nsanquer2017@22-UC27-25>
2018-06-05 09:20:28 +0200

    Natacha

---
+++ Roazhon-Fest/Controllers/ThemeController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Roazhon_Fest.Models;
+using BO;
+
+namespace Roazhon_Fest.Controllers
+{
+    public class ThemeController : Controller
+    {
+        // GET: Theme
+        public ActionResult Index()
+        {
+            List<ThemeViewModel> themesVM = new List<ThemeViewModel>();
+            List<Theme> themes = ServiceTheme.getAll();
+            foreach (Theme theme in themes)
+            {
+                themesVM.Add(new ThemeViewModel(theme));
+            }
+
+            return View(themesVM);
+        }
+
+        // GET: Theme/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Theme/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Theme/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Theme/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Theme/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Theme/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Theme/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Roazhon-Fest/Models/ThemeViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using BO;
+using Service;
+
+namespace Roazhon_Fest.Models
+{
+    public class ThemeViewModel : ViewModel<Theme>
+    {
+        public ThemeViewModel()
+        {
+            this.Metier = new Theme();
+
+        }
+        public ThemeViewModel(Theme l)
+        {
+            this.Metier = l;
+
+        }
+        
+        [Display(Name = "Theme")]
+        public string Theme
+        {
+            get
+            { return Metier.Libelle; }
+            set { Metier.Libelle = value; }
+        }
+        public static List<ThemeViewModel> GetAll()
+        {
+            List<ThemeViewModel> retour = new List<ThemeViewModel>();
+            List<Theme> livres = ServiceTheme.GetAll();
+            foreach (Theme li in livres)
+            {
+                retour.Add(new ThemeViewModel(li));
+            }
+            return retour;
+        }
+
+        public void Save()
+        {
+            if (this.ID == Guid.Empty)
+            {
+                //insert
+                ServiceTheme.Insert(this.Metier);
+            }
+            else
+            {
+                //update
+                ServiceTheme.Update(this.Metier.ID);
+            }
+        }
+
+        /// <summary>
+        /// retourne un livre ViewModel
+        /// </summary>
+        /// <param name="id">Identifiant nullable du livre</param>
+        /// <returns>si id null, retourne un viewModel avec un livre initialisé; Si id a une valeur retourne le viewModel avec le livre en BDD
+        /// </returns>
+        public static ThemeViewModel Get(Guid? id)
+        {
+            ThemeViewModel retour = null;
+
+            if (id.HasValue)
+            {
+                retour = new ThemeViewModel(ServiceTheme.Get(id.Value));
+            }
+            else
+            {
+                Theme l = new Theme() { ID = Guid.Empty, Libelle = "Default" };
+                retour = new ThemeViewModel(l);
+            }
+
+            return retour;
+        }
+    }
+}
